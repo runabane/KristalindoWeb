37d6943c62c98f815985e9edb9d7c76a1f0e85bd
runabane <santaklausmann@gmail.com>
2020-10-10 18:10:20 +0200

    init

---
+++ src/MainPages/HomePage.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+
+
+function Home(){
+    return (
+        <div>
+            <p>
+                Hello World uheuheuehe
+            </p>
+        </div>
+    )
+}
+
+export default Home;
